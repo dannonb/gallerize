a2fbecd4d1fc9392a5cfbaa32ca7381ea0c36d5d
Dannon Bigham <dannon@Dannons-MacBook-Pro.local>
2024-09-30 21:02:57 -0700

    adding

---
+++ components/landing/landing-heading.tsx
@@ -0,0 +1,8 @@
+export default function LandingHeading() {
+    return (
+        <div>
+            <h2 className="text-6xl text-center font-bold w-2/3 mx-auto my-4">Make images the fun part of <span className="bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text">web development.</span></h2>
+            <p className="p-2 text-xl text-center">Our platform allows developers to collaborate with their clients to ensure site updates are a simpler process.</p>
+        </div>
+    )
+}
