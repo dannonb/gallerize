a485b04ccb548304572f673d40f47b37ef91b601
Dannon Bigham <dannon@Dannons-MacBook-Pro.local>
2024-10-07 23:59:10 -0700

    added github signin

--- auth.ts
+++ auth.ts
@@ -1,14 +1,14 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter";
-
 
 import prisma from "./lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [Google, Github],
   pages: {
-    signIn: '/auth/login'
-  }
+    signIn: "/auth/login",
+  },
 });
