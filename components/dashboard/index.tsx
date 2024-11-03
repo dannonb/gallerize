89c2975cdd1920d267e024112f4f56fca0cb768a
dannon <53214621+dannonb@users.noreply.github.com>
2024-11-03 10:21:50 -0800

    adding vscode suggested fix for api key page component not having the correct props

--- components/dashboard/index.tsx
+++ components/dashboard/index.tsx
@@ -1,10 +1,10 @@
 import NotFoundPage from "./not-found";
 
-import UploadPage from "./overview/upload"
+import UploadPage from "./overview/upload";
 import ImagesPage from "./overview/images";
-import GalleriesPage from './overview/galleries'
+import GalleriesPage from "./overview/galleries";
 
-import GeneralSiteSettingsPage from "./settings/general"
+import GeneralSiteSettingsPage from "./settings/general";
 import APIKeysPage from "./settings/api-keys";
 import AccessPage from "./settings/access";
 import IntegrationsPage from "./settings/integrations";
@@ -14,26 +14,26 @@ import UploadingImagesDocs from "./documentation/uploading-images";
 import APIDocsPage from "./documentation/api";
 
 interface Indexable {
-    [key: string]: any;
+  [key: string]: any;
 }
 
 export const components: Indexable = {
-    // Overview Components
-    "upload": <UploadPage />,
-    "images": <ImagesPage />,
-    "galleries": <GalleriesPage />,
+  // Overview Components
+  upload: <UploadPage />,
+  images: <ImagesPage />,
+  galleries: <GalleriesPage />,
 
-    // Documentation Components
-    "uploading-images": <UploadingImagesDocs />,
-    "api": <APIDocsPage />,
-    // Settings Components
-    "general": <GeneralSiteSettingsPage />,
-    "access": <AccessPage />,
-    "api-keys": <APIKeysPage />,
-    "integrations": <IntegrationsPage />,
-    "support": <SupportPage />,
-    "advanced": <AdvancedSettingsPage />,
+  // Documentation Components
+  "uploading-images": <UploadingImagesDocs />,
+  api: <APIDocsPage />,
+  // Settings Components
+  general: <GeneralSiteSettingsPage />,
+  access: <AccessPage />,
+  "api-keys": <APIKeysPage params={undefined} session={undefined} />,
+  integrations: <IntegrationsPage />,
+  support: <SupportPage />,
+  advanced: <AdvancedSettingsPage />,
 
-    // Not found component as fallback
-    "notFound": <NotFoundPage />
-}
+  // Not found component as fallback
+  notFound: <NotFoundPage />,
+};
